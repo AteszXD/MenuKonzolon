8630ab09ad328f31831383735bbaf491791a4f61
AteszXD <keria@kkszki.hu>
2025-09-11 13:21:36 +0200

    A menü mőködése kész

--- MenuKonzolon/Program.cs
+++ MenuKonzolon/Program.cs
@@ -12,7 +12,7 @@ namespace MenuKonzolon
         {
             string nev = "";
             bool ferfi = true;
-            int szulEv;
+            int szulEv = 0;
             string valasz = "";
             do
             {
@@ -29,15 +29,31 @@ namespace MenuKonzolon
 
                 if (valasz == "1")
                 {
-                    
+                    Console.Write("Kérem a nevét: ");
+                    nev = Console.ReadLine();
+
+                    Console.Write("Kérem a születési évét: ");
+                    szulEv = int.Parse(Console.ReadLine());
+
+                    Console.Write("Kérem a nemét (Férfi/Nő): ");
+                    ferfi = Console.ReadLine() == "Férfi";
+
                     Console.WriteLine("Sikeresen rögzítettük az adatokat. Enterre tovább....");
                     Console.ReadLine();
+
                 } else if (valasz == "2")
                 {
+                    Console.WriteLine($"A neve: {nev}\n" +
+                        $"A neme: {ferfi}\n" +
+                        $"A születési éve: {szulEv}");
+
+                    Console.WriteLine("Enterre tovább....");
+                    Console.ReadLine();
 
                 } else if (valasz == "3")
                 {
-                    
+                    Console.WriteLine("Biztosan ki szeretne lépni? (Igen/Nem): ");
+                    valasz = Console.ReadLine() != "Igen" ? " " : "3";
                 }
                 else
                 {
